484faccfd45628749746e7e837c7d60bcee41d17
webstuff <dunham.scott@gmail.com>
2013-04-16 15:47:28 -0700

    Adding query motifs for the bin status page. They assume the new fully-qualified structure for the bins collection (which has not been implemented yet) so they are commented out for now

--- collection-app/lib/server/queries.js
+++ collection-app/lib/server/queries.js
@@ -15,5 +15,18 @@ module.exports = function(store) {
     store.query.expose('bin_defs', 'onlyBuildings', function(){
         return this.only('Building');
     });
+
+    //Returns bins where the most recent activity == full
+    //Assumes the new fully-qualified structure for bins, which doesn't exist yet (commenting out for now)
+    /*store.query.expose('bins', 'onlyFull', function(){
+        return this.where('data.recent.activity').equals('full');
+    });*/
+
+    //Returns bins where the most recent activity time is less than or equal to the number of specified days ago
+    //Assumes the new fully-qualified structure for bins, which doesn't exist yet (commenting out for now)
+    /*store.query.expose('bins', 'onlyOld', function(numDays){
+        var day = 24 * 3600 * 1000;
+        return this.where('data.recent.time').lte(new Date(new Date() - numDays * day));
+    });*/
 };
 
