9d891fc08394647e3190e3da5727ba8668d2c523
webstuff <dunham.scott@gmail.com>
2013-04-17 10:10:27 -0700

    Helper functions moved to /app/index.js, so removing /server/helpers.js

--- collection-app/lib/server/helpers.js
+++
@@ -1,8 +0,0 @@
-//Helper function to replace underscores with spaces (used for building names)
-view.fn('underToSpace', function(value){
-    return value && value.replace(/_/g, ' ');
-});
-
-view.fn('dashToSlash', function(value){
-    return value && value.relace(/-/g, '/');
-});
